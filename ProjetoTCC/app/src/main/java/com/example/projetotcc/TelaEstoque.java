e55bd9fccd09c3e0b0a55455aeccbf8044769e18
Mariano-Lucas <wilianbenjamin14@gmail.com>
2022-09-15 16:24:28 -0300

    Comecei tela estoque

---
+++ ProjetoTCC/app/src/main/java/com/example/projetotcc/TelaEstoque.java
@@ -0,0 +1,15 @@
+package com.example.projetotcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaEstoque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_estoque);
+
+    }
+}
